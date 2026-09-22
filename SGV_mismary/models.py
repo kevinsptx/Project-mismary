ff60385023465cfa1040972885ab46cf16b40303
gironsanchezvalentina80-sketch <gironsanchezvalentina80@gmail.com>
2026-09-22 10:36:26 -0500

    ventas1

--- SGV_mismary/models.py
+++ SGV_mismary/models.py
@@ -7,4 +7,12 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nombre
-    
+
+
+class Venta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return f"Venta de {self.cliente} - ${self.valor_total}"
--- SGV_mismary/url.py
+++ SGV_mismary/url.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('',views.home, name='home'),
+    path('', views.home, name='home'),
     path('register_cliente/', views.register_cliente, name='register_cliente'),
     path('list_cliente/', views.list_cliente, name='list_cliente'),
     path('details_cliente/<int:id>', views.cliente_details, name='details_cliente'),
-    path('cliente_update/<int:id>',views.cliente_update,name='cliente_update'),
-    path('cliente_delete/<int:id>',views.cliente_delete,name='cliente_delete')
+    path('cliente_update/<int:id>', views.cliente_update, name='cliente_update'),
+    path('cliente_delete/<int:id>', views.cliente_delete, name='cliente_delete'),
+    path('register_venta/', views.register_venta, name='register_venta'),
+    path('ventas/', views.list_venta, name='list_venta'),
+    path('details_venta/<int:id>', views.venta_details, name='details_venta'),
+    path('venta_update/<int:id>', views.venta_update, name='venta_update'),
+    path('venta_delete/<int:id>', views.venta_delete, name='venta_delete'),
 ]
