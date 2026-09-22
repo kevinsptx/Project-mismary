5e18a254f977b5f77342611b1cdbcd5f71fa3de0
kevinsptx <kevinsptx123@gmail.com>
2026-09-22 09:27:15 -0500

    usuario

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import UserLoginView
+from . import views
 
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('usuarios/', views.list_usuario, name='list_user'),
+    path('usuarios/crear/', views.register_usuario, name='register_user'),
+    path('usuarios/editar/<int:id>/', views.usuario_update, name='user_update'),
+    path('usuarios/eliminar/<int:id>/', views.usuario_delete, name='user_delete'),
+    path('usuarios/detalle/<int:id>/', views.usuario_details, name='user_details'),
 ]
